548d86981e3f7cba715c4b8d3147541d5586f2e2
Chetan baliyan <chetanbaliyan10@gmail.com>
2024-04-14 14:29:03 +0530

    feat: add ping pong

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::{
     io::{Read, Write},
     net::{TcpListener, TcpStream},
+    thread::spawn,
 };
 
+const MESSAGE_SIZE: usize = 1024;
+
 fn handle_client(mut stream: TcpStream) {
-    let mut buffer = [0; 1024];
+    let mut buffer = [0; MESSAGE_SIZE];
 
     stream
         .read(buffer.as_mut())
@@ -14,11 +17,23 @@ fn handle_client(mut stream: TcpStream) {
 
     println!("Request: {}", request);
 
-    let response = "Yoooo".as_bytes();
+    let response = b"+PONG\r\n";
 
-    stream.write(response).expect("Failed to write to stream");
+    stream
+        .write_all(response)
+        .expect("Failed to write to stream");
 }
 
 fn main() {
-    print!("Hello")
+    let listner = TcpListener::bind("127.0.0.1:8080").expect("Failed to bind to address");
+    println!("Server started at 127.0.0.1:8080");
+    listner.incoming().for_each(|stream| match stream {
+        Ok(stream) => {
+            println!("New client connected");
+            std::thread::spawn(|| handle_client(stream));
+        }
+        Err(e) => {
+            eprintln!("Error: {}", e);
+        }
+    });
 }
